9e6231c5404a71d16a452f78bbd00bbfd7ae5b05
cmarlin <cyril.marlin@gmail.com>
2026-05-17 21:22:00 +0200

    [Fix] install

--- open_spiel/scripts/install.sh
+++ open_spiel/scripts/install.sh
@@ -101,7 +101,7 @@ function cached_clone() {
 
 DIR="./pybind11"
 if [[ ! -d ${DIR} ]]; then
-  cached_clone -b smart_holder --single-branch --depth 1 https://github.com/pybind/pybind11.git ${DIR}
+  cached_clone -b archive/smart_holder --single-branch --depth 1 https://github.com/pybind/pybind11.git ${DIR}
 fi
 
 # The official https://github.com/dds-bridge/dds.git seems to not accept PR,
